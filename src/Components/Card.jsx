929ae444df5c869e4b24b07113f857c001068be4
BOMMAKAPARANDHAMAREDDY <149357107+BOMMAKAPARANDHAMAREDDY@users.noreply.github.com>
2024-02-16 13:21:22 +0530

    completed lab react css framework

---
+++ src/Components/Card.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const Card = () => {
+    return (
+        <div className="w-2/6 border h-[150px] border-gray-100 flex justify-evenly items-center gap-5 rounded" style={{ boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px" }}>
+            <img src="https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/front-end-web-development/Kalvium-Logo.png" alt="Kalvium Logo" />
+            <div>
+                <h3 className="text-xl">Kalvium Store</h3>
+                <p className="text-gray-500">You have a new course!</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card
